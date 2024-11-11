0649b33e026f9de057a3de275f1a0cf80d16ba4e
So-Ryeong Gwon <135301957+SoRrrrrrr@users.noreply.github.com>
2024-11-11 15:48:23 +0900

    Delete project1_chatbot directory

--- project1_chatbot/traning.py
+++
@@ -1,9 +0,0 @@
-import random
-import json
-import pickle
-import numpy as np # 아니 이미 있는데 왜 안됨 킹받게
-import nltk
-# stem : 단어를 원형(R)으로 만드는 것 / Lemmatizer : 표제어 추출
-from nltk.stem import WordNetLemmatizer
-
-from tensorflow.keras.models import Sequential
