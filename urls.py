42897ef8aadb681106d6044d96efb08cbf5b5949
SoRyeong Kwon <etrhffma4567@gmail.com>
2025-01-22 15:26:25 +0900

    diary history

---
+++ urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+## add new API endpoint
+from diary_app.views import analyze_diary, get_diary_entries
+
+urlpatterns = [
+    #path("admin/", admin.site.urls),
+    path('analyze/',analyze_diary), # 일기 내용을 서버로 전송하고 감정 분석 결과 반환
+    path('diary_entries/',get_diary_entries), # 서버에 저장된 모든 일기 기록을 조회
+    path('', include('diary_app.urls')) # include app's urls.py
+]
